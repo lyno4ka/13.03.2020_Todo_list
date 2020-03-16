b592b980dfe0843afc1e31cce11522fced8f73e7
lyno4ka <yuliyamasterenko@gmail.com>
2020-03-16 20:22:08 +0200

    add delete task

--- js/function.js
+++ js/function.js
@@ -1,5 +1,14 @@
 function addTask(id, task) {
-    const $taskItem = $('<li>').addClass('list-group-item').text(task.title);
+
+    const $btnDelete = $('<button>')
+            .addClass('btn btn-danger btn-xs pull-right btn-delete')
+            .html('<i class="glyphicon glyphicon-trash"></i>');
+
+    const $taskItem = $('<li>')
+            .addClass('list-group-item')
+            .text(task.title)
+            .attr('data-id', id)
+            .append($btnDelete);
 
     $tasksList
         .find(`[data-status="${task.status}"]`)
--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $formAddTask.on('submit', function(event) {
     let task = {
         id: new Date().getTime(), 
         title: $('[name="title"]', this).val(),
-        status: 2 //1 - todo, 2 - in progress, 3 - done
+        status: 1 //1 - todo, 2 - in progress, 3 - done
     };
 
     addTask(task.id, task);
@@ -25,9 +25,21 @@ console.log(localStorage);
 for (let key in localStorage) {
    
     if(!localStorage.hasOwnProperty(key)) continue;
-    console.log(key);
 
     const task = JSON.parse(localStorage[key]);
 
     addTask(task.id, task);
+    console.log(key);
 }
+
+$('body').on('click', '.btn-delete', function(event) {
+    console.log(this);
+
+    const $parent = $(this).parents('[data-id]')
+
+    // console.log($parent.attr('data-id'));
+    const taskId = $parent.data('id');
+    $parent.remove();
+
+    localStorage.removeItem(taskId);
+});
